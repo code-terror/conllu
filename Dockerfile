d52bc7318da4e2b4214c6e4aee0f4642a1bb0eb7
Venkata Krishna Yaswanth Maturi <vmaturi@sundevils.asu.edu>
2022-05-31 18:51:34 -0700

    first

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM fuzzers/atheris:1.0.3-python3.8
+WORKDIR /
+COPY . /
+RUN pip install -r requirements-dev.txt
---
+++ fuzzme.py
@@ -0,0 +1,23 @@
+import atheris
+import sys
+import os
+from conllu import parse, parse_tree
+
+def TestOneInput(data):
+    data1 = " The quick brown fox jumps over the lazy dog."
+    barray=bytearray(data)
+    fdp=atheris.FuzzedDataProvider(data)
+    if len(barray)>0:
+        if barray[0]%3==0:
+            sentences=parse(fdp.ConsumeString(len(data)))
+        if barray[0]%3==1:
+            sentences=parse_tree(fdp.ConsumeString(len(data)))
+        else:
+            sentences=parse(data1)
+            sentences=filter(form=fdp.ConsumeString(len(data)))
+    else:
+        sentences=parse(fdp.ConsumeString(len(data)))
+
+
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
